0bc1b27e52a5f0dc0cc3f330136724fb40eef8df
Dima <dima290012007@yandex.ru>
2025-01-22 22:56:01 +0300

    Get data from front

--- src/front.py
+++ src/front.py
@@ -1,13 +1,18 @@
+import time
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 
 class Window:
 	def __init__(self):
+		self.flag_ready = False
 		self.PAD = 3
 		self.WIDTH = 40
+		self.months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Декабрь']
 		self.root = tk.Tk()
 		self.root.title('Подсчет ЗП')
+		self.root.resizable(False, False)
 		# self.root.geometry('500x500')
 
 		self.frm = tk.Frame(self.root)
@@ -19,7 +24,7 @@ class Window:
 		tk.Label(self.frm, text='Путь к Bitrix').grid(column=0, row=3, sticky='w', padx=self.PAD, pady=self.PAD)
 
 		self.api = tk.Entry(self.frm, width=self.WIDTH)
-		self.month = ttk.Combobox(self.frm, width=self.WIDTH-3)
+		self.month = ttk.Combobox(self.frm, width=self.WIDTH-3, values=self.months)
 		self.excel_path = tk.Entry(self.frm, width=self.WIDTH)
 		self.bitrix_path = tk.Entry(self.frm, width=self.WIDTH)
 		self.api.grid(column=1, row=0, padx=self.PAD, pady=self.PAD)
@@ -27,10 +32,24 @@ class Window:
 		self.excel_path.grid(column=1, row=2, padx=self.PAD, pady=self.PAD)
 		self.bitrix_path.grid(column=1, row=3, padx=self.PAD, pady=self.PAD)
 
-		self.str_btn = tk.Button(self.frm, text='Запуск')
+		self.str_btn = tk.Button(self.frm, text='Запуск', command=self.ready)
 		self.str_btn.grid(column=0, row=4, columnspan=2, padx=self.PAD, pady=self.PAD)
 
-		self.root.mainloop()
-
 	def get_data(self):
-		pass
+		data = (self.api.get(), self.month.get(), self.excel_path.get(), self.bitrix_path.get())
+		self.flag_ready = False
+		if '' in data:
+			messagebox.showerror(message='Вы не указали один из параметров')
+			return False
+		else:
+			return data
+
+	def ready(self):
+		self.flag_ready = True
+
+	def get_response_ready(self):
+		while not self.flag_ready:
+			time.sleep(0.1)
+
+	def mainloop(self):
+		self.root.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
+import time
 import back
+import front
+import threading as th
+
+
+def main():
+	window.get_response_ready()
+	data = window.get_data()
+	print(data)
+
+
 if __name__ == '__main__':
-	pass
+	window = front.Window()
+	th.Thread(target=main, daemon=True).start()
+	window.mainloop()
+
 # excel = back.Excel(r'D:\Dima\Python\ABE\beginning.xlsx')
 # print(excel.get_jobs_and_div())
 # bit = back.Bitrix(r'D:\Dima\Python\ABE\Bitrix24_hours.xlsx')
