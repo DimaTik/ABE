7c12743249ced692f6e7948a11bef6fc9fea7b19
Dima <dima290012007@yandex.ru>
2025-01-25 22:38:48 +0300

    Add func get_income_of_project_in_month and add id in list of projects

--- src/back.py
+++ src/back.py
@@ -5,6 +5,9 @@ import datetime
 import pprint
 import time
 
+months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь',
+		  'Декабрь']
+
 
 class Consultant:
 	def __init__(self):
@@ -23,22 +26,23 @@ class Consultant:
 		self.soup = bs4.BeautifulSoup(src, 'lxml')
 
 	def get_standard_hours(self, month):
-		months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-		quarters = {'Январь': 0, 'Февраль': 0, 'Март': 0, 'Апрель': 1, 'Май': 1, 'Июнь': 1, 'Июль': 2, 'Август': 2, 'Сентябрь': 2, 'Ноябрь': 3,	'Октябрь': 3, 'Декабрь': 3}
+		quarters = {'Январь': 0, 'Февраль': 0, 'Март': 0, 'Апрель': 1, 'Май': 1, 'Июнь': 1, 'Июль': 2, 'Август': 2,
+					'Сентябрь': 2, 'Ноябрь': 3, 'Октябрь': 3, 'Декабрь': 3}
 		quarter = quarters[month]
 		duration = {
 			'40': 0,
 			'36': 1,
 			'24': 2
 		}
-		quarter_list = self.soup.find('div', class_='block-print').find_next_sibling().find_next_sibling().\
-			find_all('div', class_='row') 			# Нашли divs
-		quarter_list_result = [quarter_list[4]] 	# Нашли первый квартал
-		quarter_list = quarter_list[5:]				# Обрезали ввиду верстки
+		quarter_list = self.soup.find('div', class_='block-print').find_next_sibling().find_next_sibling(). \
+			find_all('div', class_='row')  # Нашли divs
+		quarter_list_result = [quarter_list[4]]  # Нашли первый квартал
+		quarter_list = quarter_list[5:]  # Обрезали ввиду верстки
 		for i in range(5, len(quarter_list), 6):
 			quarter_list_result.append(quarter_list[i])
-		standard_hours = quarter_list_result[quarter].find_all_next('div', class_='col-md-3 col-xs-2')[months.index(month)].text\
-			.split()[duration['40']].strip() 	# Магия и ты гений
+		standard_hours = \
+		quarter_list_result[quarter].find_all_next('div', class_='col-md-3 col-xs-2')[months.index(month)].text \
+			.split()[duration['40']].strip()  # Магия и ты гений
 		return standard_hours
 
 
@@ -61,30 +65,49 @@ class Bitrix:
 		self.data = {}
 
 	def get_hours(self):
-		for row in range(3, self.worksheet.max_row-1):
-			self.data[self.worksheet[row][1].value] = [self.worksheet[row][2].value, [dict([(int(self.worksheet[2][i].value[:4]), self.worksheet[row][i].value)]) for i in range(3, self.worksheet.max_row-3)]]
+		for row in range(3, self.worksheet.max_row - 1):
+			self.data[self.worksheet[row][1].value] = [self.worksheet[row][2].value, [
+				dict([(int(self.worksheet[2][i].value[:4]), self.worksheet[row][i].value)]) for i in
+				range(3, self.worksheet.max_row - 3)]]
 		return self.data
 
 	def get_projects_in_month(self):
 		return [int(self.worksheet[2][i].value[:4]) for i in range(3, self.worksheet.max_row - 3)]
 
 
-class Adesk: 	# После получения доступа к данным, попробовать вытащить контрагента
+class Adesk:  # После получения доступа к данным, попробовать вытащить контрагента
 	def __init__(self, api):
 		# '415b6479c8df4d619ff3e957e6a262242f5c3fa6024744c2ac1ff532e8d76a1e'
 		self.API = api
 		self.data = {}
 
-	def get_project(self):
+		response = requests.get(f'https://api.adesk.ru/v1/transactions?api_token={self.API}')
+		while not response.ok:
+			response = requests.get(f'https://api.adesk.ru/v1/projects?api_token={self.API}')
+			time.sleep(1)
+		self.income_json = response.json()
+
+	def get_projects(self):
 		response = requests.get(f'https://api.adesk.ru/v1/projects?api_token={self.API}')
 		while not response.ok:
 			response = requests.get(f'https://api.adesk.ru/v1/projects?api_token={self.API}')
-			time.sleep(2)
+			time.sleep(1)
 		data_json = response.json()
-		for project in range(len(data_json['projects'])):
+		for i in range(len(data_json['projects'])):
 			pprint.pprint(data_json)
-			numbers_of_project = data_json['projects'][project]['name'][:4]
-			incomes = float(data_json['projects'][project]['income'])
-			plan_incomes = float(data_json['projects'][project]['planIncome'])
+			numbers_of_project = data_json['projects'][i]['name'][:4]
+			id_of_project = data_json['projects'][i]['id']
+			incomes = float(data_json['projects'][i]['income'])
+			plan_incomes = float(data_json['projects'][i]['planIncome'])
 			self.data[numbers_of_project] = (incomes, plan_incomes)
 		return self.data
+
+	def get_income_of_project_in_month(self, month, year, id_proj):
+		income = 0
+		start = datetime.datetime.strptime(f"1.{months.index(month)+1}.{year}", "%d.%m.%Y")
+		end = datetime.datetime.strptime(f"31.{months.index(month)+1}.{year}", "%d.%m.%Y")
+		for i in range(self.income_json['recordsTotal']):
+			now = datetime.datetime.strptime(self.income_json['transactions'][i]['date'], "%d.%m.%Y")
+			if (self.income_json['transactions'][i]['project']['id'] == id_proj) and (start <= now <= end):
+				income += float(self.income_json['transactions'][i]['amount'])
+		return income
